a824eb10c280b0ed85bd3f0c61e805e92a731773
MD-MEHBUB-UL-ISLAM <jibonislamxxx@gmail.com>
2025-07-10 14:34:53 +0600

    first commit

---
+++ src/task-management-operation/task-management-operation.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-management-operation',
+  imports: [],
+  templateUrl: './task-management-operation.html',
+  styleUrl: './task-management-operation.css'
+})
+export class TaskManagementOperation {
+
+}
